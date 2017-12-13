97b71b7d601a792427998829ea3553ac9186a4ed
Simone Di Giorgi <simonetomi@gmail.com>
2017-12-13 13:34:10 +0100

    IA e carcontroller script

--- Assets/Scripts/IA.cs
+++ Assets/Scripts/IA.cs
@@ -5,44 +5,44 @@ using DG.Tweening;
 
 public class IA : MonoBehaviour {
 
-    public enum Directions { horizontal, vertical }
+    public enum Directions { HORIZONTAL, VERTICAL }
+    public GameManager GM;
+    private AudioSource source;
+    private Rigidbody rb;
 
+    [Header("Select Directions")]
+    [Space(10)]
     public Directions directions;
 
-    public GameManager GM;
+    [Header("Speed and Positions")]
+    public float speed;
+    public float spawnPosition;                                                 // Pooling Positions
 
-    private Rigidbody rb;
-    private AudioSource source;
+    [Header("Audio and Volume")]
+    public AudioClip crashSound;
+    public float volume = 0.3f;
     private bool hasAudioTriggered;
 
-    public bool isMoving;
-
-    public float speed;
-
-    // Pooling Positions
-    
-    public float spawnPosition;
-
-
-    public AudioClip crashSound;
-    private float volume = 0.3f;
+    [Header("(DEBUG)")]
+    public bool isMoving = true;
 
     void Start () {
 
-        isMoving = true;
         source = GetComponent<AudioSource>();
-        rb = GetComponent<Rigidbody>();
+
 	}
 
     private void Update()
     {
-        if (directions == Directions.vertical && isMoving)
+
+
+        if (directions == Directions.VERTICAL && isMoving)
         {
             transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
             
         }
 
-        if (directions == Directions.horizontal && isMoving)
+        if (directions == Directions.HORIZONTAL && isMoving)
         {
             transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
         }
@@ -69,13 +69,13 @@ public class IA : MonoBehaviour {
         if (other.gameObject.tag == "Traffico")
         {
 
-            if (directions == Directions.vertical && isMoving)
+            if (directions == Directions.VERTICAL && isMoving)
             {
                 transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - spawnPosition);
 
             }
 
-            if (directions == Directions.horizontal && isMoving)
+            if (directions == Directions.HORIZONTAL && isMoving)
             {
                 transform.position = new Vector3(transform.position.x - spawnPosition, transform.position.y, transform.position.z);
             }
